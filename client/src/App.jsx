9e3a4b769a2c83a09ff3634bad7dab43fd73bf16
SlemJosh <joshua.slem@gmail.com>
2024-02-14 15:39:13 -0700

    Added sandlamp background video.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
       <StoreProvider>
         <div className="app-container">
           <video autoPlay loop muted className="background-video">
-            <source src="/VideoBackground.mp4" type="video/mp4" />
+            <source src="/sandlamp.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
          <Outlet />
